a54db193ecf3f32d4fdcb91e93315e5a767a291b
Tony Pujals <tpujals@gmail.com>
2023-04-14 19:52:55 -0700

    next example with jest test

---
+++ learning/nextjs/demo-with-tests/src/pages/index.tsx
@@ -0,0 +1,29 @@
+import { Inter } from 'next/font/google'
+
+import {add} from "../lib/lib";
+
+const inter = Inter({ subsets: ['latin'] })
+
+type Props = {
+  sum: number
+}
+
+interface Data {
+  sum: number
+}
+
+export async function getServerSideProps() {
+  let sum = add(1, 2);
+  return {
+    props: {sum: sum},
+  }
+}
+
+
+export default function Home({sum}: Data) {
+  return (
+    <>
+      1 + 2 = {sum}
+    </>
+  )
+}
