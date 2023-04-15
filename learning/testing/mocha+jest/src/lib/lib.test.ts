0ec760e0655afcb2654a1e6e5024ab1eac6f2908
Tony Pujals <tpujals@gmail.com>
2023-04-14 19:27:03 -0700

    restructure & refactor

---
+++ learning/testing/mocha+jest/src/lib/lib.test.ts
@@ -0,0 +1,5 @@
+import {add} from "./lib";
+
+test("add two numbers", () => {
+  expect(add(1,2)).toBe(3);
+});
---
+++ learning/testing/mocha+jest/src/main.ts
@@ -0,0 +1,26 @@
+import { argv } from 'node:process';
+
+import {add} from "./lib/lib";
+
+// first arg is path to node
+// second arg is path to the module
+// ensure there are at least two additional args to sum
+if (argv.length < 4) {
+  console.log("Call program with two numbers");
+  process.exit(1);
+}
+
+let args = argv.slice(2);
+let sum = N(args[0]!);
+const n = args.length - 1;
+
+for (let i = 1; i <= n; i++) {
+   sum = add(sum, N(args[i]!));
+}
+
+console.log(sum);
+
+
+function N(s: string): number {
+  return parseInt(s, 10);
+}
